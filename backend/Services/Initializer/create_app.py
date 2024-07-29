859860e7a00bcb7c316f15c1ee2711912bbeb4e5
samiribrh <samiribrahimov2277@gmail.com>
2024-07-29 12:34:23 +0400

    Secret key changed to environment variable

--- backend/Services/Initializer/create_app.py
+++ backend/Services/Initializer/create_app.py
@@ -1,14 +1,19 @@
 """Module to create Flask app"""
+import os
+
 from Services.Initializer.define_blueprints import define_blueprints
 from flask_jwt_extended import JWTManager
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     """Function to create flask app with configurations"""
     app = Flask(__name__)
 
-    app.config['JWT_SECRET_KEY'] = 'akeycouldbemaximumthismuchsafe'
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     jwt = JWTManager(app)
 
     app = define_blueprints(app)
