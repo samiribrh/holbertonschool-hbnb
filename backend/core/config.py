1e69c27fff5efa81c5833faf2c7a2a76a2178369
samiribrh <samiribrahimov2277@gmail.com>
2024-08-08 16:16:20 +0400

    countries data file included

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,15 +8,23 @@ load_dotenv("../")
 
 
 class Config:
+    # Database configuration
     DBTYPE = os.getenv('DBTYPE', 'postgresql+psycopg2')
     DBUSER = os.getenv('POSTGRES_USER', 'root')
     DBPASS = os.getenv('POSTGRES_PASSWORD', 'password')
     HOSTNAME = os.getenv('HOSTNAME', 'localhost')
     DBNAME = os.getenv('POSTGRES_DB', 'database')
-    DBPORT = os.getenv('DBPORT', 5432)
+    DBPORT = int(os.getenv('DBPORT', 5432))
 
+    # SQLAlchemy configuration
     SQLALCHEMY_DATABASE_URL = f"{DBTYPE}://{DBUSER}:{DBPASS}@{HOSTNAME}:{DBPORT}/{DBNAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT configuration
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret_key')
-    STATIC_FOLDER = os.getenv('STATIC_FOLDER', 'app/static')
-    COUNTRYFILE = os.getenv('COUNTRYFILE', 'app/data/countries.json')
+
+    # Static file path
+    STATIC_FOLDER = '/app/static'
+
+    # Country file path
+    COUNTRYFILE = '/app/data/countries.json'
